885ac8c59a4b49a6659273cbfb3cd67c64659895
s.jaferpour <s.jaferpour@JAFERPOUR-PC>
2023-01-21 17:03:21 +0330

    add SelectUnitService

---
+++ ManyToMany/ManyToMany/Dtos/SelectCoursesRequest.cs
@@ -0,0 +1,10 @@
+﻿using ManyToMany.Entities;
+
+namespace ManyToMany.Dtos
+{
+    public class SelectCoursesRequest
+    {
+        public Student Student { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
---
+++ ManyToMany/ManyToMany/Services/SelectUnitService.cs
@@ -0,0 +1,45 @@
+﻿using ManyToMany.Dtos;
+using ManyToMany.Entities;
+using ManyToMany.Repositories;
+using Microsoft.Identity.Client;
+
+namespace ManyToMany.Services
+{
+    public class SelectUnitService
+    {
+        private readonly StudentRepository _studentRepository;
+        private readonly CourseRepository _courseRepository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public SelectUnitService(StudentRepository studentRepository, CourseRepository courseRepository, UnitOfWork unitOfWork) 
+        {
+            this._studentRepository = studentRepository;
+            this._courseRepository = courseRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Execute(SelectCoursesRequest request)
+        {
+            var student = new Student
+            {
+                Name = request.Student.Name,
+            };
+            
+            var stusentCourses = new List<StudentCourse>();
+            foreach (var course in request.Courses)
+            {
+                //var c = new StudentCourse
+                //{
+                //    Student = student,
+                //    CourseId = course.Id
+                //};
+                //stusentCourses.Add(c);
+                stusentCourses.Add(new StudentCourse {Student = student, CourseId = course.Id });
+            }
+            student.StudentCourses = stusentCourses;
+
+            _studentRepository.Insert(student);
+
+            _unitOfWork.CommitChanges();
+        }
+    }
+}
